6a7cd85b64581c77bbe61d081f796bd70bd12bb5
vargarenata813 <vargarenata813@gmail.com>
2023-08-05 13:39:17 +0200

    1st commit

---
+++ CodeYard/My_friends.py
@@ -0,0 +1,17 @@
+my_friends = ["Peter", "Jim", "Olivia", "Chris", "Kevin", "Linda"]
+
+print(len(my_friends))
+
+my_friends.remove("Kevin")
+
+print(my_friends)
+
+my_friends.insert(2, 'Kathleen')
+print(my_friends)
+
+print(len(my_friends))
+
+my_friends.pop()
+
+print(my_friends)
+
