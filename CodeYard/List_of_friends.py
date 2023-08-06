17185a0f4c9118508e6e4899996ed416a8b623f7
vargarenata813 <vargarenata813@gmail.com>
2023-08-06 18:13:40 +0200

    1st commit

---
+++ CodeYard/List_of_friends.py
@@ -0,0 +1,11 @@
+friends = ["Linda", "Peter", "Christine", "Melissa", "Sam", "Adrienne", "Max"]
+
+print(len(friends))
+
+friends.append("Livia")
+
+print(friends)
+
+friends.sort()
+
+print(friends)
