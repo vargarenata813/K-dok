672e227187817fc1830f1a97e666fe585756df3c
vargarenata813 <vargarenata813@gmail.com>
2023-08-07 15:51:13 +0200

    1st commit

---
+++ CodeYard/Another_list.py
@@ -0,0 +1,15 @@
+words = ["door", "apple", "house", "home", "curtain", "window", "coat", "house"]
+
+print(words.count("house"))
+
+words.pop()
+
+print(words)
+
+words.remove("apple")
+
+print(words)
+
+words.sort()
+
+print(words)
