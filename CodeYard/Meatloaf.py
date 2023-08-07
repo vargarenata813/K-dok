553dc4dfa45de0006bd31dfca7c7be5e9a4fa31f
vargarenata813 <vargarenata813@gmail.com>
2023-08-07 15:32:00 +0200

    1st commit

---
+++ CodeYard/Meatloaf.py
@@ -0,0 +1,7 @@
+print("\033[1m" + "How to open a link in Python" + "\033[0m")
+
+import webbrowser
+
+webbrowser.open("https://www.youtube.com/watch?v=9X_ViIPA-Gc")
+
+#You don't have to do anything. YouTube will open and play the video for you.
