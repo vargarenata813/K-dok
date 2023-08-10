c854de758ab2e5cf5de6c781c20810463d43dcf1
vargarenata813 <vargarenata813@gmail.com>
2023-08-10 15:41:23 +0200

    1st commit

---
+++ CodeYard/Type_conversion.py
@@ -0,0 +1,15 @@
+integer_number = 456
+float_number = 4.56
+
+new_number = integer_number + float_number
+
+print(new_number)
+
+print(type(new_number))
+
+num_1 = "12"
+num_2 = 45
+
+sum = int(num_1) + num_2
+
+print(sum)
