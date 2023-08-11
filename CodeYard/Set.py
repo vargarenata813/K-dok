34c3d41ce9b49f932e14438bb248264bb721105e
vargarenata813 <vargarenata813@gmail.com>
2023-08-11 18:14:06 +0200

    1st commit

---
+++ CodeYard/Set.py
@@ -0,0 +1,17 @@
+set1 = {58, 32, 34, 66, 98, 100}
+print(type(set1))
+print(set1)
+
+set1.add(55)
+print(set1)
+set1.discard(66)
+print(set1)
+
+set1.remove(32)
+print(set1)
+set1.pop()
+print(set1)
+
+set1.add(234)
+print(set1)
+
